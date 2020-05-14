5d67cd2e84c7ca1b4c77d4f9d9985c763a55bffb
Chandrashekhar Kotekar <chandrashekhar.kotekar@centrica.com>
2020-05-14 12:42:28 +0100

    code updated and more comments added

--- src/test/scala/com/shekhar/hadoop/DummyTest.scala
+++ src/test/scala/com/shekhar/hadoop/DummyTest.scala
@@ -1,6 +1,6 @@
 package com.shekhar.hadoop
 
-import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.{FSDataOutputStream, FileSystem, Path}
 import org.scalatest.{Matchers, WordSpec}
 
 /**
@@ -14,7 +14,31 @@ class DummyTest extends WordSpec with Matchers with HdfsWrapper {
       val fileToCreate: Path = new Path("parent/directory/dummy.txt")
       fs.create(fileToCreate, true)
       fs.exists(fileToCreate) shouldBe true
+
+      val parentDirPath: String = "parent/directory/sub/directory/"
+      val fileName: String = "new-file.txt"
+      val fileWithContents: Path = new Path(s"$parentDirPath/$fileName")
+      writeDataFileToHdfs(fs, new Path(parentDirPath), fileName, "write it to fil eon hdfs")
+      fs.exists(fileWithContents)
     }
   }
 
+  /**
+   * Companion method which simply writes data to a file on HDFS
+   * @param fs HDFS object. It can be retrieved from mini DFS cluster object
+   * @param dir directory on HDFS in which file must be written
+   * @param fileName Name of file to be written on HDFS
+   * @param text content to be written in file.
+   */
+  private def writeDataFileToHdfs(fs: FileSystem, dir: Path, fileName: String, text: String): Unit = {
+    val path: Path = dir.suffix("/" + fileName)
+    fs.mkdirs(path.getParent)
+    val out: FSDataOutputStream = fs.create(path, true)
+    out.writeBytes(text)
+    out.flush()
+    out.close()
+    // Some times mini DFS cluster takes time to write file and test cases might fail
+    // So lets sleep thread for a moment before we continue
+    Thread.sleep(1000)
+  }
 }
--- src/test/scala/com/shekhar/hadoop/HdfsWrapper.scala
+++ src/test/scala/com/shekhar/hadoop/HdfsWrapper.scala
@@ -6,24 +6,28 @@ import org.apache.hadoop.hdfs.MiniDFSCluster
 import org.scalatest.{BeforeAndAfterAll, TestSuite}
 
 /**
- * This trait wraps creation of Hadoop mini cluster and makes it available to all of its sub classes
+ * This trait wraps creation of Hadoop mini cluster
+ * Any Test class can extend from this and use mini cluster
  */
 trait HdfsWrapper extends TestSuite with BeforeAndAfterAll {
-
+  // dfsCluster object will hold reference to Mini HDFS cluster and
+  // it can be used by sub classes
   protected var dfsCluster: MiniDFSCluster = _
-  private val hdfsRootDirPath: String =
-    new Path(getClass.getClassLoader.getResource("hdfs-root/test.txt").getPath).getParent.toString
 
   override def beforeAll(): Unit = {
     super.beforeAll()
     this.dfsCluster = getHdfsCluster
   }
 
+  /**
+   * Creates HDFS configuration and creates Mini DFS cluster
+   * @return Object of type MiniDFSCluster
+   */
   private def getHdfsCluster: MiniDFSCluster = {
     val hdfsConf: Configuration = new Configuration()
-    val clusterBuilder: MiniDFSCluster.Builder = new MiniDFSCluster.Builder(hdfsConf)
+    val numberOfDataNodes: Int = 1
+    val clusterBuilder: MiniDFSCluster.Builder =
+      new MiniDFSCluster.Builder(hdfsConf).numDataNodes(numberOfDataNodes)
     clusterBuilder.build()
   }
-
-
 }
